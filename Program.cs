4b812884f9d547ba5d4b55ba4de54385b9b3bc9b
hug.sastrepro@gmail.com <hug.sastrepro@gmail.com>
2020-10-09 13:10:20 +0200

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_GitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello, bonjour , Guten tag , bonjourno");
+
+        }
+    }
+}
